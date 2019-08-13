1ffd8e2876ba7213cd665321cd0ed45821dd75ff
Ralf Popescu <ralf@landinglion.com>
2019-08-12 23:46:11 -0400

    Updates

---
+++ precache-manifest.29edc5ca9255b7f35fa613d875c383c1.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f02bd73a9aeb7b0e8e4519ffee37c8e0",
+    "url": "/ramblings/index.html"
+  },
+  {
+    "revision": "cec10ef0c1f825822249",
+    "url": "/ramblings/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "bce858e580d91eb34f12",
+    "url": "/ramblings/static/js/2.4e0d8210.chunk.js"
+  },
+  {
+    "revision": "cec10ef0c1f825822249",
+    "url": "/ramblings/static/js/main.8a33605f.chunk.js"
+  },
+  {
+    "revision": "e3f425abcea904e1b601",
+    "url": "/ramblings/static/js/runtime~main.6cc0a99a.js"
+  },
+  {
+    "revision": "d24aefbac81650b1974a5333f3cd0c2d",
+    "url": "/ramblings/static/media/1.d24aefba.png"
+  },
+  {
+    "revision": "5c6fce74b6b1cf8c157977f0ad96f9c9",
+    "url": "/ramblings/static/media/10.5c6fce74.png"
+  },
+  {
+    "revision": "3aed845d74892b7245225f9631d1ec4d",
+    "url": "/ramblings/static/media/11.3aed845d.png"
+  },
+  {
+    "revision": "86d2e4e0aeac9f7ca7916736c883ba88",
+    "url": "/ramblings/static/media/12.86d2e4e0.png"
+  },
+  {
+    "revision": "59b16a86df97f77b5a97b78a6c0c93d3",
+    "url": "/ramblings/static/media/13.59b16a86.png"
+  },
+  {
+    "revision": "0d6ec4feb861ad1b582b9144917b57f2",
+    "url": "/ramblings/static/media/14.0d6ec4fe.png"
+  },
+  {
+    "revision": "0fece275a972a306772ff43aeceae36c",
+    "url": "/ramblings/static/media/15.0fece275.png"
+  },
+  {
+    "revision": "a38e7ec7eec82b766c7446887b47c84b",
+    "url": "/ramblings/static/media/16.a38e7ec7.png"
+  },
+  {
+    "revision": "6480b68cccd0d64ab15ac8cccb4ad9dc",
+    "url": "/ramblings/static/media/17.6480b68c.png"
+  },
+  {
+    "revision": "23bb7114023d2bd302118cdbed1ca9f6",
+    "url": "/ramblings/static/media/18.23bb7114.png"
+  },
+  {
+    "revision": "e667f7cf6a06b9382efdf8784814e61f",
+    "url": "/ramblings/static/media/19.e667f7cf.png"
+  },
+  {
+    "revision": "b7323c201cd9ff352239a31a83cfd81d",
+    "url": "/ramblings/static/media/2.b7323c20.png"
+  },
+  {
+    "revision": "b2d5675a9b89392cf2a53651c31eac96",
+    "url": "/ramblings/static/media/20.b2d5675a.png"
+  },
+  {
+    "revision": "dee83c9ba6b58e44668d9bbfde03c626",
+    "url": "/ramblings/static/media/3.dee83c9b.png"
+  },
+  {
+    "revision": "9b4166bac9e17073b39b1cd0b11d3a3f",
+    "url": "/ramblings/static/media/4.9b4166ba.png"
+  },
+  {
+    "revision": "9bce1331035127c6b4dcecc0046ccae7",
+    "url": "/ramblings/static/media/5.9bce1331.png"
+  },
+  {
+    "revision": "0e0bd0af9e4e91626dca36e5d0a431e9",
+    "url": "/ramblings/static/media/6.0e0bd0af.png"
+  },
+  {
+    "revision": "828eabee3c0c7a272e8fa1c50e43ebaf",
+    "url": "/ramblings/static/media/7.828eabee.png"
+  },
+  {
+    "revision": "6bcedd7ae6f92be9a84cb823454379af",
+    "url": "/ramblings/static/media/8.6bcedd7a.png"
+  },
+  {
+    "revision": "4c986dbbd251bdfc2d8c041ff7f9669a",
+    "url": "/ramblings/static/media/9.4c986dbb.png"
+  }
+]);
